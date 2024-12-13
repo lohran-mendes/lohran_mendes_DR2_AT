8fe99435c873b6936b1e0a6122458953fe50bc8c
Lohran Mendes <lohranfm@gmail.com>
2024-12-13 10:18:56 -0300

    chore: questions 12 and 13 solved

--- script_12.js
+++ script_12.js
@@ -3,3 +3,25 @@
 // Uma fábrica produz vários tipos de doces que são enviados para as lojas em caixas de vários tipos e tamanhos. 
 // Construa um programa que obtenha a quantidade de doces fabricados e informe quantas caixas completas vão ser necessárias e se será necessário uma caixa adicional para os doces que restaram.
 // Distribuir os doces por caixas grandes (com 40 doces), médias (com 30 doces) e pequenas (com 20 doces).
+const quantidadeDoces = Number(prompt('quantidade de doces'));
+
+const extensoCaixas = ['pequenas', 'médias', 'grandes'];
+const numeroCaixas = [20, 30, 40];
+let textoFinal = `${quantidadeDoces} doces em:`;
+let ou = ''
+
+if (quantidadeDoces > 20) {
+  for (let i = 0; i < numeroCaixas.length; i++) {
+    if (quantidadeDoces % numeroCaixas[i] == 0) {
+      textoFinal += ` ${ou} ${quantidadeDoces / numeroCaixas[i]} caixa(s) ${extensoCaixas[i]}`;
+      ou = 'ou';
+    } else if (quantidadeDoces / numeroCaixas[i] > 1 && quantidadeDoces % numeroCaixas[i] > 0) {
+      textoFinal += ` ${ou} ${Math.floor(quantidadeDoces / numeroCaixas[i])} caixa(s) ${extensoCaixas[i]} e 1 ${extensoCaixas[i == 0 ? i : i - 1].slice(0, -1)} adicional`
+      ou = 'ou'
+    }
+  }
+  alert(textoFinal)
+} else if (quantidadeDoces > 0) {
+  textoFinal += ` uma caixa pequena`;
+  alert(textoFinal)
+} else alert('O valor inserido não é válido.')
--- script_13.js
+++ script_13.js
@@ -1,3 +1,5 @@
 // Sobre o Document Object Model
 
 // Crie um programa simples que mostre um alert pergunta: “O que é o DOM e para que ele serve” e em seguida mostre um outro alert com a resposta.
+alert('O que é o DOM e para que ele serve')
+alert('Dom é a parte escrita de uma aplicação web que representa o seus elementos que fazem parte do documento html ou xml.')
