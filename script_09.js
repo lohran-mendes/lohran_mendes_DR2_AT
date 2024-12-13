1b688a1eda639138015f6e253e8857c64f4d5ed4
Lohran Mendes <lohranfm@gmail.com>
2024-12-13 09:13:06 -0300

    chore: questions 9, 10 and 11 solved

--- script_09.js
+++ script_09.js
@@ -1,3 +1,9 @@
 // Sobre Funções e Bibliotecas
 
 // Crie um programa simples que mostre um alert pergunta: “O que são funções e bibliotecas?” e em seguida mostre um outro alert com a resposta.
+
+function alertQuestion() {
+  alert("O que são funções e bibliotecas?");
+  alert("funções são blocos de códigos que podem ser reutilizados, bibliotecas é um conjunto de funções e códigos focado em resolver certos problemas ou agregar certas funcionalidades.")
+}
+alertQuestion()
--- script_11.js
+++ script_11.js
@@ -1,3 +1,15 @@
 // Bissexto
 
 // Crie um sistema que solicite um ano por prompt e informe se o ano é bissexto ou não.
+const anoPerguntado = Number(
+  prompt("Insira o ano para saber se ele é bissexto")
+);
+const anoBissexto = new Date(anoPerguntado, 1, 29);
+
+if (anoBissexto.getDate() === 29) {
+  alert(anoPerguntado + " é ano bissexto");
+} else if (anoBissexto.getDate() === 1) {
+  alert(anoPerguntado + " não é ano bissexto");
+} else {
+  alert("Insira um ano válido.");
+}
