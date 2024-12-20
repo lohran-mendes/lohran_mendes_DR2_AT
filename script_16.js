04cca2425daec1cffba5a7c79544b4446a1a5661
Lohran Mendes <lohranfm@gmail.com>
2024-12-19 22:50:44 -0300

    chore: question 16 solved -fix: replace submit input with button input

--- script_16.js
+++ script_16.js
@@ -5,3 +5,22 @@
 // Exiba o resultado (número de dias de diferença) na página HTML.
 // Adicione uma validação que verifique se as datas inseridas são válidas e se a data final é maior que a data inicial. Se não forem, exiba uma mensagem de erro no HTML.
 // Assim que terminar, salve todos os links para os exercícios realizados em PDF nomeando o arquivo conforme a regra “nome_sobrenome_DR2_AT.PDF” e poste como resposta ao AT.
+function formataData(data) {
+  const arrayData = data.split('-')
+  return arrayData
+}
+function calcular(event) {
+  event.preventDefault()
+  const texto = document.querySelector('#validacao')
+  const dataInput1 = document.querySelector('#date1');
+  const dataInput2 = document.querySelector('#date2');
+  const arrayData1 = formataData(dataInput1.value)
+  const arrayData2 = formataData(dataInput2.value)
+  const data1 = new Date(arrayData1[0], arrayData1[1], arrayData1[2])
+  const data2 = new Date(arrayData2[0], arrayData2[1], arrayData2[2])
+  const diferencaEntreDatas = data2 - data1;
+  const dias = ((diferencaEntreDatas / 1000) / 3600) / 24;
+  if (dias >= 0) {
+    texto.textContent = `Há ${dias} dia(s) de diferença entre as datas.`
+  } else texto.textContent = `As datas inseridas são inválidas para a comparação.`
+}
